d8ec7c4efddfcc668f88beb67de1230096eb251c
Kendrick Schuettler <keschuettler@gmail.com>
2017-04-24 11:25:46 -0400

    Set up node server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,28 @@
 // server.js
 
-// modules ========================
-var express = require('express')
+// modules ================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+
+var app = express();
+
+//configuration ===========================================
+
+// database files
+var db = require('./data/db.js')
+
+// configure our server with all the routing
+require('./config/routes.js')(app, express);
+
+// connect to mongo database named "JoeyBagODonuts"
+mongoose.connect('mongodb://localhost/JoeyBagODonuts')
+
+// set port
+var port = process.env.PORT || 8080;
+
+// start listening to requests on port
+app.listen(port);
+
+// export app for flexibility, required by index.js
+module.exports = app;
