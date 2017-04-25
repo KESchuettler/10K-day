196e5538e3a79a22cb0688886da53bcc9172e2db
Kendrick Schuettler <keschuettler@gmail.com>
2017-04-25 15:19:21 -0400

    Completed styling

--- client/app/users/users.js
+++ client/app/users/users.js
@@ -10,7 +10,7 @@ angular.module('tenKDay.users', [
 
     // Calculate 10Kday and return to user and add to user object
     var birthday = new Date(JSON.parse(JSON.stringify(user)).birthday);
-    user.tenKday = new Date(birthday.setDate(birthday.getDate() + 0)).toDateString();
+    user.tenKday = new Date(birthday.setDate(birthday.getDate() + 10000)).toDateString();
     $scope.display = user.tenKday;
 
     // Strip mobile number in prep for Twilio
