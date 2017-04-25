dbf3656aca894eb0bbe9ea352a60548a9548e2ca
Kendrick Schuettler <keschuettler@gmail.com>
2017-04-25 18:43:05 -0400

    Changed Cron job to 8:00pm

--- workers/twilioSender.js
+++ workers/twilioSender.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 var Users = require('../server/users/userModel');
 
 exports.job = new CronJob({
-	cronTime: '0 7 * * *',
+	cronTime: '0 18 * * *',
 	onTick: function() {
     User.queueMessages()
 	},
