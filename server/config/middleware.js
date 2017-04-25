f0d6d434545f6defcaf0578385eb0b6bd04e404c
Kendrick Schuettler <keschuettler@gmail.com>
2017-04-25 17:02:36 -0400

    Changed middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,6 +2,7 @@
 // modules ==============================================
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var path = require('path')
 
 // export middleware
 
@@ -9,5 +10,6 @@ module.exports = (app, express) => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../client'));
+  app.use(express.static(path.join(__dirname, '/../../client')));
+  app.use(express.static(path.join(__dirname, '../../node_modules')));
 }
